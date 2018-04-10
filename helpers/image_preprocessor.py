e504827082c0e293956f508acf1c53fc3fd6650e
Greg Attra <greg.attra@gmail.com>
2018-04-10 19:45:14 -0400

    added resizing and padding funcs to helper

--- helpers/image_preprocessor.py
+++ helpers/image_preprocessor.py
@@ -1,22 +1,29 @@
-
+# image preprocessor object for cleaning input data
+import numpy as np
+from skimage.transform import resize as ski_resize
 
 class ImagePreprocessor:
 
-    def __init__(self, image):
-        self.image = image
-
-    def crop_image(self):
-        image_shape = self.image.shape
+    @staticmethod
+    def square_crop_image(image):
+        image_shape = image.shape
         [long_side, short_side] = [image_shape[0], image_shape[1]] if image_shape[0] > image_shape[1] else [image_shape[1], image_shape[0]]
-        indent_size = (long_side - short_side) / 2
-        cropped_image = self.image[indent_size:(long_side - indent_size), :, :] if long_side == image_shape[0] else self.image[:, indent_size:(long_side - indent_size), :]
+        indent_size = int((long_side - short_side) / 2)
+        cropped_image = image[indent_size:(long_side - indent_size), :, :] if long_side == image_shape[0] else image[:, indent_size:(long_side - indent_size), :]
 
         return cropped_image
 
-    def pad_image(self, pad_type, pad_size):
+    @staticmethod
+    def zero_pad(inputs, pad_size, is_batch=True):
+        if is_batch:
+            padded = np.pad(inputs, ((0, 0), (pad_size, pad_size), (pad_size, pad_size), (0, 0)), 'constant', constant_values=0)
+        else:
+            padded = np.pad(inputs, ((pad_size, pad_size), (pad_size, pad_size), (0, 0)), 'constant', constant_values=0)
 
-        return
+        return padded
 
-    def resize_image(self, desired_size):
+    @staticmethod
+    def resize_image(image, new_size):
+        resized_image = ski_resize(image, (new_size[0], new_size[1]))
 
-        return
+        return resized_image
