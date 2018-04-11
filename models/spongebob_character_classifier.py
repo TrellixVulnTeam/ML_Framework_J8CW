b72e2560bb4b954553e6f49ed73ed89c48daec73
Greg Attra <greg.attra@gmail.com>
2018-04-10 20:07:06 -0400

    added more cnn functions

--- models/spongebob_character_classifier.py
+++ models/spongebob_character_classifier.py
@@ -4,7 +4,15 @@ import numpy as np
 
 class SpongebobCharacterClassifier:
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, x_train, y_train):
+        self.x_train = x_train
+        self.y_train = y_train
 
+    def train(self):
+        # preprocess inputs (x)
+        self.__preprocess_x_train()
+
+    def __preprocess_x_train(self):
+        x_train = self.x_train
+
+        return True
