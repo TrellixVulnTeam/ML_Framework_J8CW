581cea4d36486188f93468288d7f2842b6616e8e
Greg Attra <greg.attra@gmail.com>
2018-04-15 12:38:05 -0400

    added loading func to data preproc service

--- models/data_model.py
+++ models/data_model.py
@@ -4,10 +4,10 @@ from services.data_preprocessor_service import DataPreprocessorService as dps
 
 class DataModel:
 
-    def __init__(self, data: dict, pad: int, image_size: list):
+    def __init__(self, data: dict, num_classes: int, pad: int, image_size: list):
         self.x_train = dps.preprocess_imageset(data['x_train'], image_size, pad)
-        self.y_train = dps.preprocess_imageset(data['y_train'], image_size, pad)
+        self.y_train = dps.one_hot_encode(data['y_train'], num_classes)
         self.x_val = dps.preprocess_imageset(data['x_val'], image_size, pad)
-        self.y_val = dps.preprocess_imageset(data['y_val'], image_size, pad)
+        self.y_val = dps.one_hot_encode(data['y_val'], num_classes)
         self.x_test = dps.preprocess_imageset(data['x_test'], image_size, pad)
-        self.y_test = dps.preprocess_imageset(data['y_test'], image_size, pad)
+        self.y_test = dps.one_hot_encode(data['y_test'], num_classes)
--- services/data_preprocessor_service.py
+++ services/data_preprocessor_service.py
@@ -1,12 +1,41 @@
 # service to preprocesses images
 import helpers.image_transform as it
+from skimage.data import imread
+from os import listdir
+import numpy as np
 
 
 class DataPreprocessorService:
 
     @staticmethod
-    def load_imagesets():
-        gary_images =
+    def load_imagesets(data_type: str):
+        imagesets = []
+        directory = 'datasets/' + data_type
+        for c, imageset in enumerate(listdir(directory)):
+            c_images = []
+            c_labels = []
+            directory += '/' + imageset
+            for image in listdir(directory):
+                c_images.append(imread(directory + '/' + image))
+                c_labels.append(c)
+
+            c_image_data = {
+                'images': c_images,
+                'labels': c_labels
+            }
+
+            imagesets.append(c_image_data)
+
+            # reset directory
+            directory = 'datasets/train'
+
+        return imagesets
+
+    @staticmethod
+    def merge_imagesets(imagesets: list):
+        merged_images = []
+        merged_labels = []
+
 
     @staticmethod
     def preprocess_imageset(imageset, image_size: list, pad):
@@ -15,3 +44,7 @@ class DataPreprocessorService:
             imageset[image] = it.resize_image(imageset[image], image_size)
 
         return imageset
+
+    @staticmethod
+    def one_hot_encode(labels, num_classes):
+        return np.eye(num_classes)[labels]
